50149d0040780e5d58a65d1d39ed770e5927bf58
Sander <hosin8388@gmail.com>
2024-09-23 17:24:28 +0330

    doing the opening and closing search area  with jQuery

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -1,38 +1,52 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    function removeAllSidebarToggleClass (){
-        
+    function removeAllSidebarToggleClass() {
+
         $('#sidebar-toggle-hide').removeClass('d-md-inline');
         $('#sidebar-toggle-hide').removeClass('d-none');
         $('#sidebar-toggle-show').removeClass('d-inline');
         $('#sidebar-toggle-show').removeClass('d-md-none');
     }
 
-    $('#sidebar-toggle-hide').click(function(){
+    $('#sidebar-toggle-hide').click(function () {
         $('#sidebar').fadeOut(300);
-        $('#main-body').animate({width : "100%"}, 300);
-        setTimeout(function(){
+        $('#main-body').animate({ width: "100%" }, 300);
+        setTimeout(function () {
             removeAllSidebarToggleClass();
             $('#sidebar-toggle-hide').addClass('d-none');
             $('#sidebar-toggle-show').removeClass('d-none');
-        },300)
+        }, 300)
     });
 
 
 
-    $('#sidebar-toggle-show').click(function(){
+    $('#sidebar-toggle-show').click(function () {
         $('#sidebar').fadeIn(300);
-        setTimeout(function(){
+        setTimeout(function () {
             removeAllSidebarToggleClass();
             $('#sidebar-toggle-hide').removeClass('d-none');
             $('#sidebar-toggle-show').addClass('d-none');
-        },300)
+        }, 300)
     })
 
 
-    $('#menu-toggle').click(function(){
+    $('#menu-toggle').click(function () {
         $('#body-header').toggle(300)
 
+    })
+
+    $('#search-toggle').click(function () {
+        $('#search-toggle').removeClass('d-md-inline');
+        $('#search-area').addClass('d-md-inline');
+        $('#search-input').animate({ width: "12rem" }, 300);
+    })
+
+    $('#search-area-hide').click(function () {
+        $('#search-input').animate({ width: "0" }, 300);
+        setTimeout(function () {
+            $('#search-toggle').addClass('d-md-inline');
+            $('#search-area').removeClass('d-md-inline');
+        }, 300)
 
     })
 })
