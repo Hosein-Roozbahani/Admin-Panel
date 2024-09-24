85f1fee3452886af94fbc105569ebdb546291ef4
Sander <hosin8388@gmail.com>
2024-09-24 17:29:37 +0330

    complete the full screen with Js

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -61,7 +61,7 @@ $(document).ready(function () {
         $('#header-profile').fadeToggle();
     })
 
-    $('.sidebar-group-link').click(function (){
+    $('.sidebar-group-link').click(function () {
 
         $('.sidebar-group-link').removeClass('sidebar-group-link-active');
         $('.sidebar-group-link').children('sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-down');
@@ -71,4 +71,62 @@ $(document).ready(function () {
         $(this).children('sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-left');
         $(this).children('sidebar-dropdown-toggle').children('.angle').addClass('fa-angle-down');
     })
+
+    // $('#full-screen').click(function () {
+
+    //     toggleFullScreen();
+    // });
+
+    // function toggleFullScreen() {
+    //     if ((document.fullscreenElement && document.fullscreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+
+    //         if (document.documentElement.requsetFullScreen) {
+    //             document.documentElement.requestFullscreen();
+    //         }
+    //         else if (document.documentElement.mozRequsetFullScreen) {
+    //             document.documentElement.mozRequestFullscreen();
+    //         }
+    //         else if (document.documentElement.webkitRequsetFullScreen) {
+    //             document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    //         }
+    //     }
+    //     $('#screen-compress').removeClass('d-none');
+    //     $('#screen-expand').addClass('d-none');
+    // }
+
+    $('#full-screen').click(function () {
+        toggleFullScreen();
+    });
+
+    function toggleFullScreen() {
+        if (!document.fullscreenElement) {
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+                document.documentElement.mozRequestFullScreen();
+            } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+                document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+            } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+                document.documentElement.msRequestFullscreen();
+            }
+            $('#screen-compress').removeClass('d-none');
+            $('#screen-expand').addClass('d-none');
+
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.mozCancelFullScreen) { // Firefox
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) { // IE/Edge
+                document.msExitFullscreen();
+            }
+            $('#screen-compress').addClass('d-none');
+            $('#screen-expand').removeClass('d-none');
+        }
+
+    }
+
+
 })
